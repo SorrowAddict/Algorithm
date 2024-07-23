840a7086a0684bda84c8f2c0e7224dbfcf35c715
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-07-23 17:02:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+N = int(input())
+lst = list(map(int, input().split()))
+cnt = 0
+for i in lst:
+    if i != 1:
+        for j in range(2, i):
+            if i%j == 0:
+                break
+        else:
+            cnt += 1
+print(cnt)
