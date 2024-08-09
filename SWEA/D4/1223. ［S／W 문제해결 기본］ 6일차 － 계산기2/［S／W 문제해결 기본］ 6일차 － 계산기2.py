c046dd09c94dfb4bf8b3f38f7922d2426e2ba21a
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-09 13:31:37 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 116 ms, Memory: 43,772 KB -BaekjoonHub

---
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -0,0 +1,38 @@
+icp, isp = {'*': 2, '+': 1}, {'*': 2, '+': 1}
+
+# Testcase 만큼 반복
+for tc in range(1, 11):
+    l = int(input())
+    N = input()
+    post_fix, stack = [], []
+
+    # ------ 후위 표기식 변환 ------ #
+    for x in N:
+        if x.isnumeric():
+            post_fix.append(x)
+        else:
+            if stack:
+                if icp[x] > isp[stack[-1]]:
+                    stack.append(x)
+                else:
+                    post_fix.append(stack.pop())
+                    stack.append(x)
+            else:
+                stack.append(x)
+    else:
+        post_fix.append(stack.pop())
+    # ------ 여기까지 ------ #
+
+    # ------ 변환식 계산 ------ #
+    stack = []
+    for x in post_fix:
+        if x.isnumeric():
+            stack.append(int(x))
+        else:
+            if x == '*':
+                stack.append(stack.pop()*stack.pop())
+            elif x == '+':
+                stack.append(stack.pop()+stack.pop())
+    # ------ 여기까지 ------ #
+
+    print(f'#{tc}', sum(stack))
