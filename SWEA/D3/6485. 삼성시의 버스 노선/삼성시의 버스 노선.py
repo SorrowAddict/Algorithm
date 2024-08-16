5f2231ba07fc3c8dd3e09bab2514cd91afb0b042
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-16 11:36:49 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 189 ms, Memory: 51,320 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,16 +1,16 @@
-# Testcase 수
 T = int(input())
-
-# Testcase 만큼 반복
 for tc in range(1, T+1):
     N = int(input())
     counts = [0] * 5001
     for _ in range(N):
-        a, b = map(int, input().split())
-        for i in range(a, b+1):
+        A, B = map(int, input().split())
+        for i in range(A, B+1):
             counts[i] += 1
 
-    lst = []
-    for _ in range(int(input())):
-        lst.append(counts[int(input())])
-    print(f'#{tc}', *lst)
+    P = int(input())
+    lst = [int(input()) for _ in range(P)]
+
+    print(f'#{tc}', end= ' ')
+    for x in lst:
+        print(counts[x], end= ' ')
+    print()
