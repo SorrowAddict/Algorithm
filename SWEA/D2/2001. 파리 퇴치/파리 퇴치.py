918a49db6c251f509b33f4e9b22040e6c56331a4
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-16 11:15:01 +0900

    [D2] Title: 파리 퇴치, Time: 133 ms, Memory: 47,960 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,17 +1,17 @@
-# Testcase 수
-T = int(input())
+# 2001. 파리 퇴치
 
-# Testcase 만큼 반복
+T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    max_v = 0
-    for i in range(N-M+1):
-        for j in range(N-M+1):
+    result = 0
+    for i in range(0, N-M+1):
+        for j in range(0, N-M+1):
             temp = 0
             for k in range(M):
                 temp += sum(arr[i+k][j:j+M])
-            max_v = max(max_v, temp)
+            if result < temp:
+                result = temp
 
-    print(f'#{tc}', max_v)
+    print(f'#{tc}', result)
