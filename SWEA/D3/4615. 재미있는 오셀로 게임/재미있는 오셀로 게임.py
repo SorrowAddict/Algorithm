f3080ae6631542056f27ae9bbdf587a87f67722f
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-16 12:16:37 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 182 ms, Memory: 51,916 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,55 +1,45 @@
+# 4615. 재미있는 오셀로 게임
+
+def f(i, j, color, N):
+    board[i][j] = color
+    for di, dj in [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,1],[1,-1],[-1,-1]]:
+        ni, nj = i+di, j+dj
+        temp = []       # 뒤집을 돌의 인덱스를 저장
+        while 0<=ni<N and 0<=nj<N and board[ni][nj]==op[color]:
+            temp.append((ni, nj))   # 뒤집을 돌을 저장
+            ni, nj = ni+di, nj+dj   # 현재 방향으로 계속 이동
+        if 0<=ni<N and 0<=nj<N and board[ni][nj] == color:
+            for p, q in temp:
+                board[p][q] = color
+
+# 1이면 흑돌, 2이면 백돌
+B, W = 1, 2
+op = [0, 2, 1]
+
 T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())    # 배열의 크기 N, 돌을 놓는 횟수 M
+    play = [list(map(int, input().split())) for _ in range(M)]
 
-for tc in range(1, T + 1):
-    N, M = map(int, input().split())
+    board = [[0]*N for _ in range(N)]   # NxN board 준비, 0 -> N-1 인덱스 사용
+    # 중심부 돌을 배치
+    # WB
+    # BW
+    board[N//2-1][N//2-1] = W
+    board[N//2][N//2-1] = B
+    board[N//2][N//2] = W
+    board[N//2-1][N//2] = B
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
+    # 돌 놓기
+    for col, row, color in play:
+        f(row-1, col-1, color, N)
 
-    # 흑돌 1 백돌 2
-    board[N // 2][N // 2] = 2
-    board[N // 2 - 1][N // 2 - 1] = 2
-    board[N // 2 - 1][N // 2] = 1
-    board[N // 2][N // 2 - 1] = 1
-
-    # 상하좌우대각선
-    dx = [0, 0, 1, -1, 1, 1, -1, -1]
-    dy = [1, -1, 0, 0, 1, -1, 1, -1]
-
-    for _ in range(M):
-        # 입력받고 좌표-1씩해서 변환해 알맞은 위치에 돌 놓기
-        i, j, color = map(int, input().split())
-        i -= 1
-        j -= 1
-        board[i][j] = color
-
-        for direction in range(8):
-            need_to_change_color = []
-            distance = 1
-            while 0 <= i + dx[direction] * distance < N and 0 <= j + dy[direction] * distance < N:
-                x = i + dx[direction] * distance
-                y = j + dy[direction] * distance
-                # 해당 방향이 빈칸이면 더 계산할 필요 없음
-                if board[x][y] == 0:
-                    break
-
-                # 해당 방향이 같은 색이면 사이에 있는 애들 전부 색깔 변환
-                if board[x][y] == color:
-                    for each_x, each_y in need_to_change_color:
-                        board[each_x][each_y] = color
-                    break
-
-                # 이도저도 아니면 일단 색깔바꿀 생각하고 다음칸 탐색하기
-                need_to_change_color.append([x, y])
-
-                distance += 1
-
-    # 칸수 세기
-    cnt_black = 0
-    cnt_white = 0
+    bcnt = wcnt = 0
     for i in range(N):
         for j in range(N):
-            if board[i][j] == 1:
-                cnt_black += 1
-            if board[i][j] == 2:
-                cnt_white += 1
-    print(f'#{tc} {cnt_black} {cnt_white}')
+            if board[i][j] == B:
+                bcnt += 1
+            elif board[i][j] == W:
+                wcnt += 1
+
+    print(f'#{tc}', bcnt, wcnt)
