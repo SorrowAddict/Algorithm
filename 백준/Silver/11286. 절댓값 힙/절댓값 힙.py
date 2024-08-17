709fdb0e5717b538f0fb1228c33f8da359bcb104
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-18 05:39:14 +0900

    [Silver I] Title: 절댓값 힙, Time: 128 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+min_v = []
+
+for _ in range(int(input())):
+    n = int(input())
+    
+    if n == 0:
+        if len(min_v):
+            print(heapq.heappop(min_v)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(min_v, (abs(n), n))
