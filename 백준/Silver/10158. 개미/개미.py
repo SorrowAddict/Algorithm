7aab29e9ffee3823a99c1cb5ee1383b0dc2e00de
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-17 23:13:28 +0900

    [Silver III] Title: 개미, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+w, h = map(int, input().split())
+x, y = map(int, input().split())
+t = int(input())
+
+x = w - abs(w - (x + t) % (2 * w))
+y = h - abs(h - (y + t) % (2 * h))
+
+print(x, y)
