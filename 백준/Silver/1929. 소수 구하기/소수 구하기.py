014744e022aa5570b1a0f2a5ebd037f44a62d440
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-25 19:40:57 +0900

    [Silver III] Title: 소수 구하기, Time: 160 ms, Memory: 41460 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,21 @@
-import sys
-input = sys.stdin.readline
+# ------ 에라토스테네스의 체 알고리즘 사용 ------ #
+def sieve_of_eratosthenes(n):
+    is_prime = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, n + 1) if is_prime[p]]
+    return prime_numbers
 
 M, N = map(int, input().split())
 
-for i in range(M, N+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5)+1):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+# N까지의 모든 소수를 한 번만 계산합니다.
+primes = sieve_of_eratosthenes(N)
+
+# [M, N] 범위 내의 소수를 필터링하여 출력합니다.
+for prime in primes:
+    if prime >= M:
+        print(prime)
