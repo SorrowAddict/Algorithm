173544a531959b0904e07e438c6c031d761cba37
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-27 21:35:46 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,19 +1,11 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-arr = deque(list(range(1, N+1)))
-print('<', end='')
-i, cnt = 0, 0
-while cnt < N:
-    temp = arr.popleft()
-    if (i+1)%K == 0:
-        cnt += 1
-        print(temp, end='')
-        if cnt != N:
-            print(', ', end='')
-    else:
-        arr.append(temp)
-    i += 1
-print('>')
+arr = list(range(1, N+1))
+result = []
+i = 0
+while arr:
+    i = (i + K - 1) % len(arr)
+    result.append(arr.pop(i))
+print('<' + ', '.join(map(str, result)) + '>')
