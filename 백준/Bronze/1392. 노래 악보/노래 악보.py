90a7293fd3d830fe52a030d61f3d883c337ba9b0
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-08-31 17:01:31 +0900

    [Bronze II] Title: 노래 악보, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1392. 노래 악보/노래 악보.py
@@ -0,0 +1,8 @@
+N, Q = map(int, input().split())
+lst = [int(input()) for _ in range(N)]
+for _ in range(Q):
+    t = int(input())
+    for i in range(N):
+        if t < sum(lst[:i+1]):
+            print(i+1)
+            break
