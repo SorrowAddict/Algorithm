35a8b20e70d9ece468d35481aa16c0aef183af91
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-06 12:53:42 +0900

    [D3] Title: 부분 수열의 합, Time: 4,564 ms, Memory: 48,700 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,3 +1,13 @@
+def permutation():
+    global ans
+    for i in range(1<<N):
+        temp = 0
+        for j in range(N):
+            if i & (1<<j):
+                temp += arr[j]
+        if temp == K:
+            ans += 1
+
 # Testcase 수
 T = int(input())
 
@@ -6,14 +16,6 @@ for tc in range(1, T+1):
     N, K = map(int, input().split())
     arr = list(map(int, input().split()))
 
-    result = 0
-    # 모든 부분 집합의 경우의 수
-    for i in range(1<<N):
-        temp = 0
-        # 찾아볼 모든 인덱스 i 값
-        for j in range(len(arr)):
-            if i & (1<<j):
-                temp += arr[j]
-        if temp == K:
-            result += 1
-    print(f'#{tc}', result)
+    ans = 0
+    permutation()
+    print(f'#{tc}', ans)
