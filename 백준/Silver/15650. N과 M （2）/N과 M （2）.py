ee685f0a208e39fc5a2a64936210a98385d279ba
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-09 01:25:50 +0900

    [Silver III] Title: N과 M (2), Time: 28 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-path = []
-def perm_recur(level, start):
+def comb_recur(level, start):
     if level == M:
         print(*path)
         return
     for i in range(start+1, N+1):
         path.append(i)
-        perm_recur(level+1, i)
+        comb_recur(level+1, i)
         path.pop()
 
 N, M = map(int, input().split())
-arr = list(range(1, N+1))
-perm_recur(0, 0)
+path = []
+comb_recur(0, 0)
