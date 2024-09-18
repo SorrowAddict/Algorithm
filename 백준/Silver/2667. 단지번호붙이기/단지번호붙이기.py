a2ab021ac756b7e71448d0d79fb8bb9f66f4d287
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-18 15:21:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -7,23 +7,23 @@ dj = (1, 0, -1, 0)
 def bfs(i, j):
     cnt = 1
     q = deque([(i, j)])
-    arr[i][j] = '0'
+    arr[i][j] = 0
     while q:
         ti, tj = q.popleft()
         for k in range(4):
             ni, nj = ti+di[k], tj+dj[k]
-            if 0<=ni<N and 0<=nj<N and arr[ni][nj] == '1':
+            if 0<=ni<N and 0<=nj<N and arr[ni][nj]:
                 cnt += 1
                 q.append((ni, nj))
-                arr[ni][nj] = '0'
+                arr[ni][nj] = 0
     lst.append(cnt)
 
 N = int(input())
-arr = [list(input()) for _ in range(N)]
+arr = [list(map(int, input().strip())) for _ in range(N)]
 lst = []
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == '1':
+        if arr[i][j]:
             bfs(i, j)
 print(len(lst))
 for i in sorted(lst):
