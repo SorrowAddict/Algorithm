77e9ecf4d5e62cd036110b5172af96d095338934
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-18 15:16:47 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+di = (0, 1, 0, -1)
+dj = (1, 0, -1, 0)
+def bfs(i, j):
+    cnt = 1
+    q = deque([(i, j)])
+    arr[i][j] = '0'
+    while q:
+        ti, tj = q.popleft()
+        for k in range(4):
+            ni, nj = ti+di[k], tj+dj[k]
+            if 0<=ni<N and 0<=nj<N and arr[ni][nj] == '1':
+                cnt += 1
+                q.append((ni, nj))
+                arr[ni][nj] = '0'
+    lst.append(cnt)
+
+N = int(input())
+arr = [list(input()) for _ in range(N)]
+lst = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == '1':
+            bfs(i, j)
+print(len(lst))
+for i in sorted(lst):
+    print(i)
