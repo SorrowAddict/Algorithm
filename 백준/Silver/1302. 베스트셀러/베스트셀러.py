7f2bd0da68aa7d186c408c81baf3f6681b9ab357
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-22 20:52:35 +0900

    [Silver IV] Title: 베스트셀러, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+d = dict()
+
+for _ in range(int(input())):
+    book = input().strip()
+    if book in d:
+        d[book] += 1
+    else:
+        d[book] = 1
+
+max = max(d.values())
+ans = []
+for k, v in d.items():
+    if v == max:
+        ans.append(k)
+
+ans.sort()
+print(ans[0])
