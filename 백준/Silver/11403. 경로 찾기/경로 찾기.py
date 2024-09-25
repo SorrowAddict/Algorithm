d717cc8f18fe6f2286c968eaa336474efd00a8a4
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-09-25 22:06:16 +0900

    [Silver I] Title: 경로 찾기, Time: 224 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][k] == 1 and arr[k][j] == 1:
+                arr[i][j] = 1
+
+for row in arr:
+    print(*row)
