a7feae95aaacbaa2472e78011f925061eaaffcb5
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2024-10-27 18:27:54 +0900

    [Silver V] Title: 숫자 카드, Time: 740 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,20 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(x):
-    start, end = 0, N-1
-    while start <= end:
-        mid = (start+end)//2
-        if arr[mid] == x:
-            return True
-        elif arr[mid] < x:
-            start = mid + 1
-        else:
-            end = mid - 1
-
 N = int(input())
-arr = sorted(list(map(int, input().split())))
+arr = list(map(int, input().split()))
 M = int(input())
 check = list(map(int, input().split()))
+
+d = dict()
+for x in arr:
+    if x not in d:d[x] = 1
 for x in check:
-    print(1 if binary_search(x) else 0, end=' ')
+    print(1 if x in d else 0, end=' ')
