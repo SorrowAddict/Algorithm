1714c4f7743359ff4e9808abb99edb8779fd3771
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2025-03-17 23:40:30 +0900

    [Bronze V] Title: A+B - 3, Time: 124 ms, Memory: 14636 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A + B + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
