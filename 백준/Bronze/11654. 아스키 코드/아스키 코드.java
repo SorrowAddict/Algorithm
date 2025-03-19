dca99180f28f0e916303a92ec222612172453985
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2025-03-20 00:25:32 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(br.read());
+        System.out.println((int) br.readLine().charAt(0));
         br.close();
     }
 }
