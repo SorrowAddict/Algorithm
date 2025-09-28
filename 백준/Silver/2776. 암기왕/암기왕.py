a6e54ad808b258bc78111ff280e28fbf0d472b88
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2025-09-28 23:25:59 +0900

    [Silver IV] Title: 암기왕, Time: 1364 ms, Memory: 228256 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    A = set(map(int, input().split()))
+    m = int(input())
+    B = list(map(int, input().split()))
+    
+    for b in B:
+        if b in A:
+            print(1)
+        else:
+            print(0)
