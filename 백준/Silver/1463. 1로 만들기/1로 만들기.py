adececcba7c5a6716c1223e6a1f580d787919d95
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2025-12-23 20:47:02 +0900

    [Silver III] Title: 1로 만들기, Time: 460 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-
-d = [0] * (N+1)
-for i in range(2, N+1):
-    d[i] = d[i-1] + 1
+dp = [0] * (N + 1)
+for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1
     if i % 3 == 0:
-        d[i] = min(d[i], d[i//3]+1)
+        dp[i] = min(dp[i], dp[i // 3] + 1)
     if i % 2 == 0:
-        d[i] = min(d[i], d[i//2]+1)
-print(d[N])
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+print(dp[N])
