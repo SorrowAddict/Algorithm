926a665a4deb777b2e5c4049dc0632d5bac6272c
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2026-02-02 23:32:55 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -6,14 +6,6 @@ def find_parent(parent, target):
         parent[target] = find_parent(parent, parent[target])
     return parent[target]
 
-def union(parent, i, j):
-    a = find_parent(parent, i)
-    b = find_parent(parent, j)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
 N = int(input())
 M = int(input())
 parent = {i: i for i in range(1, N + 1)}
@@ -21,12 +13,11 @@ graph = [list(map(int, input().split())) for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
-            union(parent, i + 1, j + 1)
+            a = find_parent(parent, i + 1)
+            b = find_parent(parent, j + 1)
+            if a != b:
+                parent[b] = a
 plan = list(map(int, input().split()))
 root = find_parent(parent, plan[0])
-for city in plan:
-    if find_parent(parent, city) != root:
-        print("NO")
-        break
-else:
-    print("YES")
+plan_parents = set(find_parent(parent, city) for city in plan)
+print("YES" if len(plan_parents) == 1 else "NO")
