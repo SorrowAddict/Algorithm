45e49ec3a99ff37b831b6130dddcff1b357f643e
Doyeong Jung <154123905+SorrowAddict@users.noreply.github.com>
2026-06-10 21:39:01 +0900

    [level 2] Title: 비밀 코드 해독, Time: 103.03 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,24 +1,19 @@
 def solution(n, q, ans):
     answer = 0
     s_q = [set(x) for x in q]
-    
-    subset = []
-    def dfs(start, level):
-        nonlocal answer
+    for i in range(1 << n):
+        subset = []
+        for j in range(n):
+            if i & (1 << j):
+                subset.append(j + 1)
+        if len(subset) != 5:
+            continue
         
-        if level == 5:
-            s_subset = set(subset)
-            for s, target in zip(s_q, ans):
-                cnt = len(s & s_subset)
-                if cnt != target:
-                    return
-            else: answer += 1
-            return
-        
-        for i in range(start, n + 1):
-            subset.append(i)
-            dfs(i + 1, level + 1)
-            subset.pop()
-            
-    dfs(1, 0)
+        s_subset = set(subset)
+        for s, target in zip(s_q, ans):
+            cnt = len(s & s_subset)
+            if cnt != target:
+                break
+        else:
+            answer += 1
     return answer
